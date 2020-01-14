95710d452123c2dec32f80c44fd339e2ab1a2016
aalspaw <alspawalex@gmail.com>
2020-01-14 13:20:46 -0600

    added post mapping

--- src/main/java/com/alexalspaw/aiapetclinic/web/BaseController.java
+++ src/main/java/com/alexalspaw/aiapetclinic/web/BaseController.java
@@ -1,6 +1,7 @@
 package com.alexalspaw.aiapetclinic.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class BaseController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello world!";
+    }
+
+    @PostMapping
+    public String helloPost() {
+        return "Posted";
     }
 }
