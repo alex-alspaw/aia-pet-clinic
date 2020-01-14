4f76a7385c06d040019aed3f062f24a33c535efb
aalspaw <alspawalex@gmail.com>
2020-01-14 12:16:07 -0600

    commit of spring-boot initializer project

---
+++ src/main/java/com/alexalspaw/aiapetclinic/AiaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.alexalspaw.aiapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiaPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexalspaw/aiapetclinic/AiaPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexalspaw.aiapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiaPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
